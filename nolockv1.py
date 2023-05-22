53b42acaea9a72ce39d37d04f8aa7380d614d29b
Pichat Nithikanjanathan <Pichat_intt@kkumail.com>
2023-05-22 16:02:37 +0700

    reduce if condition
    try to reduce complexity of "if" condition in led location control function

--- nolockv1.py
+++ nolockv1.py
@@ -4,62 +4,98 @@ from smbus import SMBus
 i2cbus = SMBus(1)  # Specify the I2C bus number (e.g., 1 for /dev/i2c-1)
 # Set the slave address by static addr
 
+"""
 def led_loc(address, led_location, color_tagging):
     if led_location == 0:
-        datalist1 = [0]  # led bytes data to sent
+        data_list1 = [0]  # led bytes data to sent
         if color_tagging == 0:
             datalist2 = [0, 0, 0]
         elif color_tagging == 1:
-            datalist2 = [255, 0, 0]
+            data_list2 = [255, 0, 0]
     elif led_location == 1:
-        datalist1 = [1]  # led bytes data to sent
+        data_list1 = [1]  # led bytes data to sent
         if color_tagging == 0:
-            datalist2 = [0, 0, 0]
+            data_list2 = [0, 0, 0]
         elif color_tagging == 1:
-            datalist2 = [255, 127, 0]
+            data_list2 = [255, 127, 0]
     elif led_location == 2:
-        datalist1 = [2]  # led bytes data to sent
+        data_list1 = [2]  # led bytes data to sent
         if color_tagging == 0:
-            datalist2 = [0, 0, 0]
+            data_list2 = [0, 0, 0]
         elif color_tagging == 1:
-            datalist2 = [255, 255, 0]
+            data_list2 = [255, 255, 0]
     elif led_location == 3:
         datalist1 = [3]  # led bytes data to sent
         if color_tagging == 0:
-            datalist2 = [0, 0, 0]
+            data_list2 = [0, 0, 0]
         elif color_tagging == 1:
-            datalist2 = [0, 255, 0]
+            data_list2 = [0, 255, 0]
     elif led_location == 4:
-        datalist1 = [4]  # led bytes data to sent
+        data_list1 = [4]  # led bytes data to sent
         if color_tagging == 0:
-            datalist2 = [0, 0, 0]
+            data_list2 = [0, 0, 0]
         elif color_tagging == 1:
-            datalist2 = [0, 0, 255]
+            data_list2 = [0, 0, 255
     elif led_location == 5:
-        datalist1 = [5]  # led bytes data to sent
+        data_list1 = [5]  # led bytes data to sent
         if color_tagging == 0:
-            datalist2 = [0, 0, 0]
+            data_list2 = [0, 0, 0]
         elif color_tagging == 1:
-            datalist2 = [75, 0, 130]
+            data_list2 = [75, 0, 130]
     elif led_location == 6:
-        datalist1 = [6]  # led bytes data to sent
+        data_list1 = [6]  # led bytes data to sent
         if color_tagging == 0:
-            datalist2 = [0, 0, 0]
+            data_list2 = [0, 0, 0]
         elif color_tagging == 1:
-            datalist2 = [148, 0, 211]
+            data_list2 = [148, 0, 211]
     elif led_location == 7:
-        datalist1 = [7]  # led bytes data to sent
+        data_list1 = [7]  # led bytes data to sent
         if color_tagging == 0:
-            datalist2 = [0, 0, 0]
+            data_list2 = [0, 0, 0]
         elif color_tagging == 1:
-            datalist2 = [255, 255, 255]
-    data = datalist1 + datalist2  # Bytes data to display
+            data_list2 = [255, 255, 255]
+    data = data_list1 + data_list2  # Bytes data to display
     time.sleep(0.15)  # display delay
     # --------------------------------
     # display debugging
     print(data)
     # --------------------------------
     i2cbus.write_i2c_block_data(address, 0, data)
+"""
+
+
+def led_loc(address, led_location, color_tagging):
+    data_list1 = [led_location]  # led bytes data to send
+    if color_tagging == 0:
+        data_list2 = [0, 0, 0]
+    elif color_tagging == 1:
+        if led_location == 0:
+            data_list2 = [255, 0, 0]
+        elif led_location == 1:
+            data_list2 = [255, 127, 0]
+        elif led_location == 2:
+            data_list2 = [255, 255, 0]
+        elif led_location == 3:
+            data_list2 = [0, 255, 0]
+        elif led_location == 4:
+            data_list2 = [0, 0, 255]
+        elif led_location == 5:
+            data_list2 = [75, 0, 130]
+        elif led_location == 6:
+            data_list2 = [148, 0, 211]
+        elif led_location == 7:
+            data_list2 = [255, 255, 255]
+
+    data = data_list1 + data_list2  # Bytes data to display
+    time.sleep(0.15)  # display delay
+
+    # --------------------------------
+    # display debugging
+    print(data)
+    # --------------------------------
+
+    i2cbus.write_i2c_block_data(address, 0, data)
+
 
 led_loc(16, 0, 1)
 led_loc(16, 1, 1)
